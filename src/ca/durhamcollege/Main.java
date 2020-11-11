f7eac3d5741d307a60972c5ceb5f1c8cc1ccb471
Nick Sturch-Flint <nick.rulez@live.com>
2020-11-11 10:32:19 -0500

    Added basic input/output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,20 @@
+/*
+ * Author      : Nick Sturch-Flint
+ * Date        : November 11, 2020
+ * Program Name: In-Class Exercise 08
+ */
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s", name);
     }
 }
