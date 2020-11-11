2fc1c80119744625a905b72d1b711db8c556e732
Nick Sturch-Flint <nick.rulez@live.com>
2020-11-11 11:25:06 -0500

    Added validation and exception handling

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -5,6 +5,7 @@
  */
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -15,14 +16,41 @@ public class Main
         String name = "";
         Scanner keyboard = new Scanner(System.in);
         float age = 0.0f;
+        boolean dirtyFlag = true;
 
         //Input
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
-        System.out.print("Please enter your age : ");
-        age = keyboard.nextFloat();
+
+        while (dirtyFlag)
+        {
+            try
+            {
+                System.out.print("Please enter your age : ");
+                age = keyboard.nextFloat();
+                //Validation
+                if (age != 0.0f)
+                {
+                    dirtyFlag = false;
+                }
+                else
+                    {
+                        System.out.print("\nYou must enter a valid float greater than 0.0\n");
+                        keyboard.nextLine();
+                        dirtyFlag = true;
+                    }
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.out.print("\nYou must enter a valid float greater than 0.0\n");
+                keyboard.nextLine();
+                dirtyFlag = true;
+            }
+        }
+
 
         //Output
+        System.out.println();
         System.out.printf("You entered: %s\n", name);
         System.out.printf("Your entered: %.2f\n", age);
     }
